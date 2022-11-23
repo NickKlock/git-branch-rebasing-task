15128504dd54140d89372b8720658247f4810238
nick <klockgether.nick@gmail.com>
2022-11-23 11:35:31 +0100

    add for loop

--- src/FeatureB.java
+++ src/FeatureB.java
@@ -2,5 +2,9 @@ public class FeatureB {
 
     public static void main(String[] args) {
         System.out.println("Das ist Feature B von: Marcus ");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
